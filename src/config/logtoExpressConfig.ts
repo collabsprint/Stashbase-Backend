272b2c5a50193f64905625ff31da9fc9b799a033
Chumexxx <obasyemeka@gmail.com>
2026-03-03 17:59:28 +0100

    .

--- src/config/logtoExpressConfig.ts
+++ src/config/logtoExpressConfig.ts
@@ -32,7 +32,7 @@ export const sessionConfig: SessionOptions = {
   secret: process.env.SESSION_SECRET || 'sessionsecret',
   cookie: { 
     maxAge: 14 * 24 * 60 * 60 * 1000,
-    secure:   process.env.NODE_ENV === 'production',
+    secure:   process.env.NODE_ENV === 'development' ? false : true,
     httpOnly: true,
     sameSite: 'none'
   },
